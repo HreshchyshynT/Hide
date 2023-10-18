06542b7720c1e0dc2bfbdb05cb0bc59c03c0f403
--global <hreshchyshyn.t@gmail.com>
2023-10-18 23:58:16 +0300

    additional tests, comment related to test flakiness

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,13 +1,15 @@
 use assert_cmd::prelude::*; // Add methods on commands
 use assert_fs::prelude::*;
 use predicates::prelude::*;
-use serde_json::{json, Value}; // Used for writing assertions
-use std::{fs, process::Command}; // Run programs
+use serde_json::Value; // Used for writing assertions
+use std::{fs, io::Write, process::Command}; // Run programs
 
 static PLACEHOLDER: &str = "[hidden]";
 
+type Result = std::result::Result<(), Box<dyn std::error::Error>>;
+
 #[test]
-fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+fn file_doesnt_exist() -> Result {
     let mut cmd = Command::cargo_bin("hide")?;
 
     cmd.arg("-i").arg("test/file/doesnt/exist");
@@ -19,13 +21,16 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[test]
-fn hide_values_in_simple_json() -> Result<(), Box<dyn std::error::Error>> {
+fn hide_values_in_simple_json() -> Result {
     let file = assert_fs::NamedTempFile::new("sample.json")?;
-    let json = json!({
+    let json: Value = serde_json::from_str(
+        r#"{
         "name": "Name",
         "surname": "Surname",
         "age": 99
-    });
+    }"#,
+    )
+    .unwrap();
 
     file.write_str(&json.to_string())?;
 
@@ -37,14 +42,20 @@ fn hide_values_in_simple_json() -> Result<(), Box<dyn std::error::Error>> {
 
     cmd.assert()
         .success()
-        .stdout(predicate::str::contains("\"name\": \"[hidden]\""))
-        .stdout(predicate::str::contains("\"surname\": \"[hidden]\""));
+        .stdout(predicate::str::contains(format!(
+            "\"name\": \"{}\"",
+            PLACEHOLDER,
+        )))
+        .stdout(predicate::str::contains(format!(
+            "\"surname\": \"{}\"",
+            PLACEHOLDER,
+        )));
 
     Ok(())
 }
 
 #[test]
-fn hide_values_in_nested_json() -> Result<(), Box<dyn std::error::Error>> {
+fn hide_values_in_nested_json() -> Result {
     let file = assert_fs::NamedTempFile::new("sample.json")?;
     let input = r#"
         {
@@ -57,16 +68,18 @@ fn hide_values_in_nested_json() -> Result<(), Box<dyn std::error::Error>> {
         }
         "#;
 
-    let expected_output = json!(
-        {
-            "user": {
-                "username": "alice",
-                "credentials": {
-                    "password": "[hidden]"
-                }
-            }
-        }
+    let expected_output = format!(
+        r#"{{
+        "user": {{
+            "username": "alice",
+            "credentials": {{
+                "password": "{}"
+            }}
+        }}
+    }}"#,
+        PLACEHOLDER,
     );
+    let expected_output: Value = serde_json::from_str(&expected_output).unwrap();
     file.write_str(input)?;
 
     let mut cmd = Command::cargo_bin("hide")?;
@@ -82,7 +95,7 @@ fn hide_values_in_nested_json() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[test]
-fn hide_values_in_json_array() -> Result<(), Box<dyn std::error::Error>> {
+fn hide_values_in_json_array() -> Result {
     let file = assert_fs::NamedTempFile::new("sample.json")?;
     let input = r#"
     [
@@ -91,10 +104,16 @@ fn hide_values_in_json_array() -> Result<(), Box<dyn std::error::Error>> {
     ]
     "#;
 
-    let expected_output = json!([
-        {"username":"alice","password":"[hidden]"},
-        {"username":"bob","password":"[hidden]"}
-    ]);
+    let expected_output = format!(
+        r#"
+    [
+        {{"username": "alice", "password": "{}"}},
+        {{"username": "bob", "password": "{}"}}
+    ]
+    "#,
+        PLACEHOLDER, PLACEHOLDER,
+    );
+    let expected_output: Value = serde_json::from_str(&expected_output).unwrap();
 
     file.write_str(input)?;
 
@@ -111,8 +130,10 @@ fn hide_values_in_json_array() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+// test is flaky if running with default "cargo test [--test cli]" because of all tests run in parallel
+// to ensure all works well use "cargo test --test cli -- --test-threads=1"
 #[test]
-fn test_storing_config() -> Result<(), Box<dyn std::error::Error>> {
+fn test_storing_config() -> Result {
     let file_path = confy::get_configuration_file_path("hide", "hide-cfg").unwrap();
     if file_path.exists() {
         fs::remove_file(&file_path).unwrap();
@@ -120,7 +141,7 @@ fn test_storing_config() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut cmd = Command::cargo_bin("hide")?;
     cmd.arg("--add-words").arg("name,surname");
-    cmd.assert().success();
+    cmd.unwrap().assert().success();
 
     assert!(file_path.exists());
 
@@ -134,8 +155,97 @@ fn test_storing_config() -> Result<(), Box<dyn std::error::Error>> {
 
     let file = fs::read_to_string(&file_path).unwrap();
     println!("file: {file}");
+    std::io::stdout().flush().unwrap();
     assert_eq!(file.contains("'name'"), false);
     assert!(file.contains("'surname'"));
 
+    Ok(())
+}
+
+#[test]
+fn test_empty_object() -> Result {
+    let file = assert_fs::NamedTempFile::new("sample.json")?;
+    let empty_object = r#"{}"#;
+    file.write_str(&empty_object)?;
+    let mut cmd = Command::cargo_bin("hide")?;
+    cmd.arg("-i").arg(file.path());
+
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("{}"));
+    Ok(())
+}
+
+#[test]
+fn test_empty_array() -> Result {
+    let file = assert_fs::NamedTempFile::new("sample.json")?;
+    let empty_array = r#"[]"#;
+    file.write_str(&empty_array)?;
+    let mut cmd = Command::cargo_bin("hide")?;
+    cmd.arg("-i").arg(file.path());
+
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("[]"));
+    Ok(())
+}
+
+#[test]
+fn test_invalid_input_json() -> Result {
+    let file = assert_fs::NamedTempFile::new("sample.json")?;
+    // Missing closing brace for the entire JSON object
+    let invalid_input = r#"{
+        "user": {
+            "username": "alice",
+            "credentials": {
+                "password": "secret",
+            } 
+    }"#;
+    file.write_str(invalid_input).unwrap();
+
+    let mut cmd = Command::cargo_bin("hide")?;
+    cmd.arg("-i").arg(file.path());
+
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("could not parse file"));
+    Ok(())
+}
+
+#[test]
+fn test_array_inside_object() -> Result {
+    let file = assert_fs::NamedTempFile::new("sample.json")?;
+    let array_inside_object = r#"{
+    "users": [
+        {"name": "Alice", "password": "secret1"},
+        {"name": "Bob", "password": "secret2"}
+    ]
+}"#;
+    file.write_str(array_inside_object).unwrap();
+    let expected_output = format!(
+        r#"
+    {{
+        "users": [
+            {{"name": "Alice", "password": "{}"}},
+            {{"name": "Bob", "password": "{}"}}
+        ]
+    }}
+    "#,
+        PLACEHOLDER, PLACEHOLDER,
+    );
+    let expected_output: Value = serde_json::from_str(&expected_output).unwrap();
+
+    let mut cmd = Command::cargo_bin("hide")?;
+    cmd.arg("-i")
+        .arg(file.path())
+        .arg("--add-words")
+        .arg("password")
+        .arg("--remove-words")
+        .arg("name"); // ensure that key 'name' is not in "to hide" list
+
+    // TODO: learn why do we need to_owned() here
+    let output = cmd.assert().success().get_output().stdout.to_owned();
+    let output: Value = serde_json::from_str(&String::from_utf8(output)?)?;
+    assert_eq!(expected_output, output);
     Ok(())
 }
